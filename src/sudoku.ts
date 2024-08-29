bcb478c8eed5dc52831b4b126fb6f35abfd65bd0
cornercrap <hikarynogo113@gmail.com>
2024-08-30 08:38:43 +0900

    解法3: 行・列・ブロックで候補が2つであるセルが同じ行・列・ブロックにある場合、その他の候補を除外する

--- src/sudoku.ts
+++ src/sudoku.ts
@@ -2,6 +2,9 @@ const SOLUTION1 = 1;
 const SOLUTION2ROW = 2;
 const SOLUTION2COL = 3;
 const SOLUTION2BLOCK = 4;
+const SOLUTION3ROW = 5;
+const SOLUTION3COL = 6;
+const SOLUTION3BLOCK = 7;
 
 class Sudoku {
   private board: Cell[][] = [];
@@ -141,6 +144,18 @@ class Sudoku {
           solutionStr =
             "解法2: ブロックで候補の数字が唯一つの場合数字が決定する";
           break;
+        case SOLUTION3ROW:
+          solutionStr =
+            "解法3: 行で候補が2つであるセルが同じ行にある場合、その他の候補を除外する";
+          break;
+        case SOLUTION3COL:
+          solutionStr =
+            "解法3: 列で候補が2つであるセルが同じ列にある場合、その他の候補を除外する";
+          break;
+        case SOLUTION3BLOCK:
+          solutionStr =
+            "解法3: ブロックで候補が2つであるセルが同じブロックにある場合、その他の候補を除外する";
+          break;
         default:
           solutionStr = "解法不明";
           break;
@@ -222,7 +237,9 @@ class Sudoku {
 
   /** 数独を解く */
   run() {
-    /** 解法1: 候補が１つしかないセルは数字が決定する */
+    /**
+     * 解法1: 候補が１つしかないセルは数字が決定する
+     */
     this.runAllCell((row, col) => {
       const candidates = this.board[row][col].getCandidates();
       if (candidates.length === 1) {
@@ -232,7 +249,9 @@ class Sudoku {
     });
     this.updateCandidates();
 
-    /** 解法2: 行・列・ブロックで候補の数字が唯一つの場合数字が決定する */
+    /**
+     * 解法2: 行・列・ブロックで候補の数字が唯一つの場合数字が決定する
+     */
     // 行の検査
     for (let row = 0; row < 9; row++) {
       let allCandidates: number[] = [];
@@ -308,8 +327,163 @@ class Sudoku {
       });
     });
     this.updateCandidates();
+
+    /**
+     * 解法3: 行・列・ブロックで候補が2つであるセルが同じ行・列・ブロックにある場合、その他の候補を除外する
+     */
+    // 行の検査
+    for (let row = 0; row < 9; row++) {
+      let allCandidates: number[] = [];
+      for (let col = 0; col < 9; col++) {
+        allCandidates.push(...this.board[row][col].getCandidates());
+      }
+      for (let num1 = 1; num1 <= 9; num1++) {
+        const count1 = allCandidates.filter((c) => c === num1).length;
+        if (count1 === 2) {
+          for (let num2 = 1; num2 <= 9; num2++) {
+            if (num1 === num2) {
+              continue;
+            }
+            const count2 = allCandidates.filter((c) => c === num2).length;
+            // 条件1: 候補数が2つの数字が２種類ある
+            if (count1 === count2) {
+              let cellCount = 0;
+              for (let col = 0; col < 9; col++) {
+                const candidates = this.board[row][col].getCandidates();
+                if (candidates.includes(num1) && candidates.includes(num2)) {
+                  cellCount++;
+                }
+              }
+              // 条件2: 同じセルに候補がある
+              if (cellCount === 2) {
+                for (let col = 0; col < 9; col++) {
+                  const candidates = this.board[row][col].getCandidates();
+                  if (
+                    candidates.includes(num1) &&
+                    candidates.includes(num2) &&
+                    candidates.length > 2
+                  ) {
+                    this.board[row][col].setCandidates([num1, num2]);
+                    this.pushSolutionHistory(
+                      row,
+                      col,
+                      num1 * 10 + num2,
+                      SOLUTION3ROW
+                    );
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // 列の検査
+    for (let col = 0; col < 9; col++) {
+      let allCandidates: number[] = [];
+      for (let row = 0; row < 9; row++) {
+        allCandidates.push(...this.board[row][col].getCandidates());
+      }
+      for (let num1 = 1; num1 <= 9; num1++) {
+        const count1 = allCandidates.filter((c) => c === num1).length;
+        if (count1 === 2) {
+          for (let num2 = 1; num2 <= 9; num2++) {
+            if (num1 === num2) {
+              continue;
+            }
+            const count2 = allCandidates.filter((c) => c === num2).length;
+            // 条件1: 候補数が2つの数字が２種類ある
+            if (count1 === count2) {
+              let cellCount = 0;
+              for (let row = 0; row < 9; row++) {
+                const candidates = this.board[row][col].getCandidates();
+                if (candidates.includes(num1) && candidates.includes(num2)) {
+                  cellCount++;
+                }
+              }
+              // 条件2: 同じセルに候補がある
+              if (cellCount === 2) {
+                for (let row = 0; row < 9; row++) {
+                  const candidates = this.board[row][col].getCandidates();
+                  if (
+                    candidates.includes(num1) &&
+                    candidates.includes(num2) &&
+                    candidates.length > 2
+                  ) {
+                    this.board[row][col].setCandidates([num1, num2]);
+                    this.pushSolutionHistory(
+                      row,
+                      col,
+                      num1 * 10 + num2,
+                      SOLUTION3COL
+                    );
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // ブロックの検査
+    this.runAllBlock((BRow, BCol) => {
+      let allCandidates: number[] = [];
+      this.runAllBlock((row, col) => {
+        allCandidates.push(
+          ...this.board[BRow * 3 + row][BCol * 3 + col].getCandidates()
+        );
+      });
+      for (let num1 = 1; num1 <= 9; num1++) {
+        const count1 = allCandidates.filter((c) => c === num1).length;
+        if (count1 === 2) {
+          for (let num2 = 1; num2 <= 9; num2++) {
+            if (num1 === num2) {
+              continue;
+            }
+            const count2 = allCandidates.filter((c) => c === num2).length;
+            // 条件1: 候補数が2つの数字が２種類ある
+            if (count1 === count2) {
+              let cellCount = 0;
+              this.runAllBlock((row, col) => {
+                const candidates =
+                  this.board[BRow * 3 + row][BCol * 3 + col].getCandidates();
+                if (candidates.includes(num1) && candidates.includes(num2)) {
+                  cellCount++;
+                }
+              });
+              // 条件2: 同じセルに候補がある
+              if (cellCount === 2) {
+                this.runAllBlock((row, col) => {
+                  const candidates =
+                    this.board[BRow * 3 + row][BCol * 3 + col].getCandidates();
+                  if (
+                    candidates.includes(num1) &&
+                    candidates.includes(num2) &&
+                    candidates.length > 2
+                  ) {
+                    this.board[BRow * 3 + row][BCol * 3 + col].setCandidates([
+                      num1,
+                      num2,
+                    ]);
+                    this.pushSolutionHistory(
+                      BRow * 3 + row,
+                      BCol * 3 + col,
+                      num1 * 10 + num2,
+                      SOLUTION3BLOCK
+                    );
+                  }
+                });
+              }
+            }
+          }
+        }
+      }
+    });
   }
 
+  /** 解法の履歴を追加 */
   private pushSolutionHistory(
     row: number,
     col: number,
